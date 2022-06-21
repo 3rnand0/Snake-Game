d4ba4f32dff61534c4365572be9de8feca3eb8fc
3rnand0 <20439@jhc.school.nz>
2022-06-21 21:28:41 +1200

    menu file

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
-#MVP https://docs.replit.com/tutorials/build-snake-with-pygame
-
 import pygame
-from snake import * 
+from snake import *
 from food import *
 
-#Creating the window for Snake  game
 pygame.init()
-bounds = (300,300)
+bounds = (500, 500)
 window = pygame.display.set_mode(bounds)
 pygame.display.set_caption("Snake")
+
 block_size = 20
 snake = Snake(block_size, bounds)
+food = Food(block_size, bounds)
+font = pygame.font.SysFont('comicsans', 80, True)
 
 run = True
 while run:
-  pygame.time.delay(100)
+	pygame.time.delay(100)
 
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
 
-  window.fill((0,0,0))
-  snake.draw(pygame, window)
-  pygame.display.flip()
-  
-  run = True
-while run:
-  pygame.time.delay(100)
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_LEFT]:
+		snake.steer(Direction.LEFT)
+	elif keys[pygame.K_RIGHT]:
+		snake.steer(Direction.RIGHT)
+	elif keys[pygame.K_UP]:
+		snake.steer(Direction.UP)
+	elif keys[pygame.K_DOWN]:
+		snake.steer(Direction.DOWN)
 
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
+	snake.move()
+	snake.check_for_food(food)
 
-  keys = pygame.key.get_pressed()
-  if keys[pygame.K_LEFT]:
-    snake.steer(Direction.LEFT)
-  elif keys[pygame.K_RIGHT]:
-    snake.steer(Direction.RIGHT)
-  elif keys[pygame.K_UP]:
-    snake.steer(Direction.UP)
-  elif keys[pygame.K_DOWN]:
-    snake.steer(Direction.DOWN)
+	if snake.check_bounds() == True or snake.check_tail_collision() == True:
+		text = font.render('Game Over', True, (255, 255, 255))
+		window.blit(text, (120, 120))
+		pygame.display.update()
+		pygame.time.delay(1000)
+		snake.respawn()
+		food.respawn()
 
-  snake.move()
-  window.fill((0,0,0))
-  snake.draw(pygame, window)
-  pygame.display.update()
+	window.fill((0, 0, 0))
+	snake.draw(pygame, window)
+	food.draw(pygame, window)
+	pygame.display.update()
--- snake.py
+++ snake.py
@@ -1,7 +1,3 @@
-#MVP https://docs.replit.com/tutorials/build-snake-with-pygame
-
-# Creating the Snake Class
-
 from enum import Enum
 
 class Direction(Enum):
@@ -9,6 +5,7 @@ class Direction(Enum):
   DOWN = 1
   LEFT = 2
   RIGHT = 3
+
 
 class Snake:
   length = None
@@ -18,19 +15,23 @@ class Snake:
   color = (0,0,255)
   bounds = None
 
+
   def __init__(self, block_size, bounds):
     self.block_size = block_size
     self.bounds = bounds
     self.respawn()
+
 
   def respawn(self):
     self.length = 3
     self.body = [(20,20),(20,40),(20,60)]
     self.direction = Direction.DOWN
 
+
   def draw(self, game, window):
     for segment in self.body:
       game.draw.rect(window, self.color, (segment[0],segment[1],self.block_size, self.block_size))
+
 
   def move(self):
     curr_head = self.body[-1]
@@ -50,6 +51,7 @@ class Snake:
     if self.length < len(self.body):
       self.body.pop(0)
 
+
   def steer(self, direction):
     if self.direction == Direction.DOWN and direction != Direction.UP:
       self.direction = direction
@@ -59,4 +61,40 @@ class Snake:
       self.direction = direction
     elif self.direction == Direction.RIGHT and direction != Direction.LEFT:
       self.direction = direction
-            
+
+
+  def eat(self):
+    self.length += 1
+
+  def check_for_food(self, food):
+    head = self.body[-1]
+    if head[0] == food.x and head[1] == food.y:
+      self.eat()
+      food.respawn()
+
+
+  def check_tail_collision(self):
+    head = self.body[-1]
+    has_eaten_tail = False
+
+    for i in range(len(self.body) - 1):
+      segment = self.body[i]
+      if head[0] == segment[0] and head[1] == segment[1]:
+        has_eaten_tail = True
+
+    return has_eaten_tail
+
+
+  def check_bounds(self):
+    head = self.body[-1]
+    if head[0] >= self.bounds[0]:
+      return True
+    if head[1] >= self.bounds[1]:
+      return True
+
+    if head[0] < 0:
+        return True
+    if head[1] < 0:
+        return True
+
+    return False
